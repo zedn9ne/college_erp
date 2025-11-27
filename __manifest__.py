ca801c934356037b51afdb451cd24da7a06aa785
Mahdi Zanganeh <mahdizedn9ne@gmail.com>
2025-11-27 16:40:00 +0330

    Initial commit: college_erp module

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'College_Erp',
+    'version': '2.2',
+    'license' : 'LGPL-3',
+    'summary': 'An ERP for college education ',
+    'description' : ' From students adminstration to exam , this covers all aspects of college adminstration . ',
+    'category': 'Education',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'author' : 'Mahdi Zanganeh',
+    'website' : 'www.zedn9ne.com',
+    'sequence' : 1 ,
+    'application' : True,
+    'auto_install' : True,
+    'installable' : True
+}
---
+++ models/college_student.py
@@ -0,0 +1,5 @@
+from odoo import models , fields
+
+class CollegeSudent(models.Model):
+    _name = "college.student"
+    _description = "College Student"
